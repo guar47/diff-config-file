909b6e3bc742213aedbafa6f1a8b216d7ed82d12
guar47 <guar47@ya.ru>
2017-01-23 19:07:12 +0300

    fix help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+const program = require('commander');
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .usage('[options] <first_config> <second_config>')
+  .arguments('<first_config> <second_config>')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
